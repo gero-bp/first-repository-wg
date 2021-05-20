60e46d9e32e0458877fd4b1a3b4fdf57ead0ba44
Geronimo <geronimo.bazan@mercadolibre.com>
2021-05-20 13:16:48 -0300

    adding my files

---
+++ Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+    
+    private Long weight;
+    private Long height;
+
+    
+    public Animal() {
+    }
+
+    public Long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Long weight) {
+        this.weight = weight;
+    }
+
+    public Long getHeight() {
+        return height;
+    }
+
+    public void setHeight(Long height) {
+        this.height = height;
+    }
+
+}
